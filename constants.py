98a8a1236eda33738171b59c57ec9a9d612825c7
lbinninger22 <lukas.binninger@gmail.com>
2025-09-02 04:25:18 +0200

    Adjusted league end year and added explanation

--- constants.py
+++ constants.py
@@ -1,6 +1,5 @@
-leagueID = "1609009"
-leagueStartYear = 2013
-leagueEndYear = 2024
+leagueStartYear = 2016
+leagueEndYear = 2025    # this will include all years up to, but not including leagueEndYear
 
 standings_directory = './output/'+ leagueID + '-history-standings/'
 gamecenter_directory = './output/'+ leagueID + '-history-teamgamecenter/'
